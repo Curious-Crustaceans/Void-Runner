c2ba66ec6ea1efd6a61ee03077b24fe70bd76bd1
AnthonyAscione <46875205+AnthonyAscione@users.noreply.github.com>
2020-02-16 17:21:12 -0600

    Updated Door Controller

---
+++ Assets/doorController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorController : MonoBehaviour
+{
+    private Animator thisAnim;
+ 
+void Start ()
+    {
+    thisAnim = GetComponent<Animator>();
+    }
+}
