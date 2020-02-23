461836550febe9896e47fbd03d783344dd7b674f
cstanier <cstanier123@gmail.com>
2020-02-23 16:35:45 -0600

    HealthBarChanges

--- Assets/Scripts/PlayerDMG.cs
+++ Assets/Scripts/PlayerDMG.cs
@@ -8,52 +8,27 @@ public class PlayerDMG : MonoBehaviour
 {
     // Start is called before the first frame update
     
-<<<<<<< Updated upstream
-    public float player_health;
-    float player_starting_health = 5;
-    public float inv;
-    float lastDamage;
-=======
-<<<<<<< Updated upstream
-    public int player_health;
-    int player_starting_health = 5;
-=======
     public float player_health;
     float player_starting_health = 10;
     public float inv;
     float lastDamage;
->>>>>>> Stashed changes
-
     public HealthBar healthBar;
     
->>>>>>> Stashed changes
 
     void Awake() {
     	player_health = player_starting_health;
         healthBar.SetMaxHealth(player_starting_health);
     }
 
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-    public void TakeDMG(int dmg) {
-    	player_health = player_health - dmg;
-        
-=======
->>>>>>> Stashed changes
     public void TakeDMG(float dmg)
     {
         if (Time.time - lastDamage >= inv) { 
         player_health -= dmg;
         lastDamage = Time.time;
-<<<<<<< Updated upstream
-    }
-=======
         
         healthBar.SetHealth(player_health);
     }
->>>>>>> Stashed changes
->>>>>>> Stashed changes
+
     }
 
     void Update(){
