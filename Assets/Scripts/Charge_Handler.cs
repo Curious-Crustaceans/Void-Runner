fb4c1d72adf24ac227f37ce401b9796c2b8fbcc7
AnthonyAscione <46875205+AnthonyAscione@users.noreply.github.com>
2020-03-01 23:37:01 -0600

    Stuff

--- Assets/Scripts/Charge_Handler.cs
+++ Assets/Scripts/Charge_Handler.cs
@@ -2,23 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Charge_Handler : MonoBehaviour
+public class Charge_Handler : EnemyMind
 {
     public int attacks = 0;
     public bool collided;
     PlayerDMG playerScript;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         collided = false;
-        playerScript = GameObject.Find("Capsule").GetComponent<PlayerDMG>();
+        playerScript = GameObject.Find("Player").GetComponent<PlayerDMG>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (active)
+            anim.SetBool("Start", true);
     }
 
     private void OnTriggerEnter(Collider other)
